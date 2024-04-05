76e43b78d83da2fd65f4efb31be495a65ba90273
Matthew Beale <mbeale@jaguarlandrover.com>
2024-04-05 16:06:20 +0100

    Quartz sync: Apr 5, 2024, 4:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mplb99.github.io/Eryndor",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
